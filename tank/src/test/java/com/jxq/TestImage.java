dd068dcc0427f602b2c31e140b14c8f12de2474d
jingxingqiang123 <1045612194@qq.com>
2020-11-09 21:51:06 +0800

    测试imageIO读取图片

---
+++ tank/src/test/java/com/jxq/TestImage.java
@@ -0,0 +1,36 @@
+package com.jxq;
+
+import org.junit.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public class TestImage {
+
+
+    @Test
+    public void testImages2() {
+        try {
+
+            BufferedImage image2 = ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader()
+                    .getResourceAsStream("images\\111.png")));
+            System.out.println(image2.getWidth());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    // 放在resource的目录下
+    @Test
+    public void testImages1() {
+        try {
+            BufferedImage image = ImageIO.read(new File(
+                    "E:\\springboot-example\\tank\\src\\main\\resources\\images\\111.png"));
+            System.out.println(image.getWidth());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
